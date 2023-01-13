c3d1c6d6be04170061635075deb510a577e19e49
Geoffrey.C <cycjimmy@gmail.com>
2023-01-13 11:43:58 +0800

    fix: test

--- release.config.js
+++ release.config.js
@@ -1,5 +1,14 @@
-module.exports = {
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+/* eslint no-template-curly-in-string: "off" */
+const makeConfig = require('@cycjimmy/config-lib/cjs/semanticRelease/15.x/makeConfig.cjs').default;
+
+module.exports = makeConfig({
   npmOptions: {
     npmPublish: false,
   },
-};
+  git: true,
+  gitAssets: [
+    'package.json',
+    'package-lock.json',
+  ],
+});
